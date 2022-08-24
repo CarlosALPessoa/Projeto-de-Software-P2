8a2f4942eaf4fd730fc5c77c6dc1b751fe533e50
CarloSALeandro <80118142+YusukeTakashi@users.noreply.github.com>
2022-08-24 11:35:10 -0300

    Add files via upload

---
+++ introducao.java
@@ -0,0 +1,7 @@
+public class introducao
+{
+    public static void main(String[] args) {
+      System.out.println("Welcome to:");
+      System.out.println("POO Java, to Project Software");
+    }
+}
